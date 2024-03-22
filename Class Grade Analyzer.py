3a90e166c4766e58c98381fb0e170f7ad443ccb4
Tomf35 <132650968+Tomf35@users.noreply.github.com>
2024-03-22 10:20:51 +0000

    Create Class Grade Analyzer.py

---
+++ Class Grade Analyzer.py
@@ -0,0 +1,10 @@
+grades = [8.5, 7.9, 6.8, 8.2, 9, 6.3, 8.4]
+count = 0
+
+# Count the number of grades above 8.0
+for grade in grades:
+    if grade > 8:
+        count += 1
+
+# Display the result
+print(count)
