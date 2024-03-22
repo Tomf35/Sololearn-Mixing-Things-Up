c5c7285336f75b808dc8fd7945b592c2fb984f0b
Tomf35 <132650968+Tomf35@users.noreply.github.com>
2024-03-22 12:07:14 +0000

    Create Temperature Statistics.py

---
+++ Temperature Statistics.py
@@ -0,0 +1,11 @@
+temperatures = [22, 25, 19, 21, 24, 28, 23]
+
+# Find the lowest and highest temperature of the week
+lowest_temp = min(temperatures)
+highest_temp = max(temperatures)
+
+# Display the lowest temperature
+print(lowest_temp)
+
+# Display the highest temperature
+print(highest_temp)
